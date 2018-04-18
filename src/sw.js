c1b141fd4539c8fc379a4d0da4e5f7fc814a0f54
Euan Goddard <euan.goddard@gmail.com>
2018-04-18 21:14:50 +0100

    Added support for service worker

---
+++ src/sw.js
@@ -0,0 +1,4 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js',
+);
+workbox.precaching.precacheAndRoute([]);
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{html,js,css}"
+  ],
+  "swDest": "dist/sw.js",
+  "swSrc": "src/sw.js"
+};
