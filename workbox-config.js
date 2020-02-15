357be6c4d4ba3bcc58c5ac6d44be7a8e7a24432d
Euan Goddard <euan@stockopedia.com>
2020-02-15 07:11:41 +0000

    Generated ES2018

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "dist/",
   "globPatterns": [
-    "**/*.{html,js,css}"
+    "**/*.{html,js,css,png,ico,webmanifest}"
   ],
   "swDest": "dist/sw.js",
   "swSrc": "src/sw.js"
