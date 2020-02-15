6a92122801f2e77da5bb3a2f2dc921a62c1988b1
Euan Goddard <euan@stockopedia.com>
2020-02-15 07:10:14 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,12 +1,36 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
 workbox.precaching.precacheAndRoute([
   {
+    "url": "android-chrome-192x192.eb2e35b9.png",
+    "revision": "b947e094736008bd6f86cd3338e45c5c"
+  },
+  {
+    "url": "android-chrome-512x512.d3cf37f8.png",
+    "revision": "5b6b8632d917c93c9452bb9dc0fea89f"
+  },
+  {
+    "url": "apple-touch-icon.86a170ed.png",
+    "revision": "23df29c91ce35ac3d3eeebe89a79cbbd"
+  },
+  {
+    "url": "favicon-16x16.b8510d1c.png",
+    "revision": "82877cee36d326da17aeadff0f19e136"
+  },
+  {
+    "url": "favicon-32x32.29acd1f1.png",
+    "revision": "d048410c56580db8f224787edeca5f99"
+  },
+  {
     "url": "index.html",
     "revision": "457627dce8a30619d5ad259f3d5f70f5"
   },
   {
     "url": "main.01a9b15c.js",
     "revision": "4b0bb13845fd968b89c5b0de17bc99eb"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "b076d5f3794e7f24a1a64464a261c50f"
   },
   {
     "url": "styles.173b00b6.css",
