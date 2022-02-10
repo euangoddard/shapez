81baac09ecdc01dce71a43003d45395923f860a8
Euan Goddard <euan@stockopedia.com>
2022-02-10 18:02:05 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,47 +1,47 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js');
 workbox.precaching.precacheAndRoute([
   {
-    "url": "android-chrome-192x192.eb2e35b9.png",
-    "revision": "b947e094736008bd6f86cd3338e45c5c"
+    "url": "android-chrome-192x192.58bbe927.png",
+    "revision": "50130e4d08b14b6bfc973a2cdf564ec6"
   },
   {
-    "url": "android-chrome-512x512.d3cf37f8.png",
-    "revision": "5b6b8632d917c93c9452bb9dc0fea89f"
+    "url": "android-chrome-512x512.9bdb3030.png",
+    "revision": "ed5408bbfaeee0bbd24cc569bf0d8714"
   },
   {
-    "url": "apple-touch-icon.86a170ed.png",
-    "revision": "23df29c91ce35ac3d3eeebe89a79cbbd"
+    "url": "apple-touch-icon.8914c274.png",
+    "revision": "a99b1610e12a04977acb41be6df8e997"
   },
   {
-    "url": "favicon-16x16.b8510d1c.png",
-    "revision": "82877cee36d326da17aeadff0f19e136"
+    "url": "favicon-16x16.25446123.png",
+    "revision": "4cd52b68754220dba4fef06be980f1f5"
   },
   {
-    "url": "favicon-32x32.29acd1f1.png",
-    "revision": "d048410c56580db8f224787edeca5f99"
+    "url": "favicon-32x32.28965fce.png",
+    "revision": "75487f64a2dec507700eb912c3b338bb"
+  },
+  {
+    "url": "index.ad4d6c65.css",
+    "revision": "3ed4baec62e40eae465c8f6eca5c8c4a"
+  },
+  {
+    "url": "index.cf7ee263.js",
+    "revision": "998ef290ce0e42c485cd2636d360e12f"
+  },
+  {
+    "url": "index.e41ac911.js",
+    "revision": "e3db3e7ce150954571dae4d9d397b320"
   },
   {
     "url": "index.html",
-    "revision": "1b97f27a595e4af2e66fad0fb8a57756"
-  },
-  {
-    "url": "main.01a9b15c.js",
-    "revision": "4b0bb13845fd968b89c5b0de17bc99eb"
+    "revision": "767c77bee6db75fa11cbb8eedc1d93e4"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "b076d5f3794e7f24a1a64464a261c50f"
+    "revision": "84444ac55921dbaff26ddc1424545732"
   },
   {
-    "url": "styles.173b00b6.css",
-    "revision": "8fde0e6bdc0221bbbbae387dcc3369ad"
-  },
-  {
-    "url": "sw-register.9a1d39be.js",
-    "revision": "c3f71b4a60c75a79e95736f01f583ae0"
-  },
-  {
-    "url": "worker.bd51d5ea.js",
-    "revision": "58b7e3a0c6b16619ce25889c7aa72588"
+    "url": "worker.176310b6.js",
+    "revision": "3a115bee978289173cd49974448d10ae"
   }
 ]);
